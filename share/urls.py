f823b2e6fc841c6ac13607defb337f03ff2539bc
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-06-23 16:14:35 +0300

    create follow and unfollow url

--- share/urls.py
+++ share/urls.py
@@ -7,6 +7,14 @@ urlpatterns=[
     path('signup/',SignUpView.as_view(),name='signup'),
     path('home',views.home, name='home'),
     path('viewProfile',views.viewProfile, name='viewProfile'),
+    path('postVideo',views.postVideo, name='postVideo'),
+    path('video/<id>',views.comment, name='comment'),
+    path('like/<id>', views.like_post,name='like'),
+    path('unfollow/<to_unfollow>', views.unfollow, name='unfollow'),
+    path('follow/<to_follow>', views.follow, name='follow'),
+
+
+
 
 
 ]
